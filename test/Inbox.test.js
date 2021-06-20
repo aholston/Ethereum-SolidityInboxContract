564d5f0c86a0b72aa403c28d2369d76aeee873ea
Andre <andreholston@Andres-MacBook-Pro-2.local>
2021-06-20 14:18:44 -0500

    Mocha tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -7,22 +7,36 @@ const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+const initialString = 'Hello World';
+const nextString = 'Message Changed';
 
 beforeEach(async () => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
         
-
     // Use one of those accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hello World'] })
+        .deploy({ 
+            data: bytecode, 
+            arguments: [initialString] 
+        })
         .send({ from: accounts[0], gas: '1000000' })
-
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.strictEqual(message, initialString);
+    });
+
+    it('sets a message', async () => {
+        await inbox.methods.setMessage(nextString).send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.strictEqual(nextString, message);
     });
 })
 
